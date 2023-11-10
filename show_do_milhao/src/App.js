95e0025c37f86a81f1b102826bbce6008a272a2c
JoaoMiguez <joaovmiguez@gmail.com>
2023-11-10 22:47:32 +0000

    use effct inicio

--- show_do_milhao/src/App.js
+++ show_do_milhao/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Ajuda from './components/Ajuda';
 import Alternativa from './components/Alternativa';
@@ -11,26 +11,37 @@ function App() {
       "pergunta": 'Qual é a capital do Brasil?',
       "alternativas": ['Rio de Janeiro', 'Brasília', 'Bahia', 'São Paulo'],
       "respostaCerta": 'Brasília',
-    }
+      "perguntaEscolhida": true,
+    },
+    
   ] //Carregar as perguntas;
   const [pergunta, setPergunta] = useState(
     {
       'pergunta': 'Qual é a capital do Brasil?',
       'alternativas': ['Rio de Janeiro', 'Brasília', 'Bahia', 'São Paulo'],
-      'respostaCerta': 'Brasília'
+      'respostaCerta': 'Brasília',
+      "perguntaEscolhida": false,
     }
   );
+
+
 
   const [perguntaAtual, setPerguntaAtual] = useState(0);
   const [alternativaSelecionada, setAlternativaSelecionada] = useState(null);
   const [indexSelecionado, setIndexSelecionado] = useState(null);
   const [alternativaCorreta, setAlternativaCorreta] = useState(false);
   
-
-
+  
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min)
+  }
+  
   function aoConfirmar() {
     if (alternativaSelecionada == pergunta.respostaCerta) {
-      
+      pergunta.perguntaEscolhida = true;
+      listaDePerguntas[index].perguntaEscolhida = pergunta.perguntaEscolhida
       console.log("Parabéns vc acertou!")
     } else {
       console.log(alternativaSelecionada == pergunta.respostaCerta)
@@ -38,14 +49,24 @@ function App() {
 
     setAlternativaCorreta(pergunta.respostaCerta);
   }
-
+  
   function aoSelecionar(opcaoSelecionada) {
     console.log(opcaoSelecionada);
     setAlternativaSelecionada(opcaoSelecionada);
     console.log(alternativaCorreta)
   }
-
-
+  
+  useEffect(() => {
+    let escolha = getRandomInt(0,listaDePerguntas.length)
+    console.log(listaDePerguntas.length)
+    while(listaDePerguntas[escolha].perguntaEscolhida){
+       escolha = getRandomInt(0,listaDePerguntas.length)
+    }
+    setPergunta(listaDePerguntas[escolha])
+    let index = escolha
+  
+  },[]);
+  
   return (
     <div className='container'>
 
