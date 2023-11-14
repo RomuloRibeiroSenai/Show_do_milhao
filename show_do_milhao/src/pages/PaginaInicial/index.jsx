b2e2f15f6c27382c57cbd2b88184c6a5476b3977
RomuloRibeiroSenai <142239254+RomuloRibeiroSenai@users.noreply.github.com>
2023-11-14 22:35:56 +0000

    2

--- show_do_milhao/src/pages/PaginaInicial/index.jsx
+++ show_do_milhao/src/pages/PaginaInicial/index.jsx
@@ -1,6 +1,8 @@
 import logo from '../../assets/logo.png';
+// import comeco from '../../assets/sons/comeco.mp3'
 import './style.css';
 import { useNavigate } from "react-router-dom";
+
 
 export default function PaginaInicial (){
 
