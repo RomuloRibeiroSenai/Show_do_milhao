26311401e1bb133641d8f8a56d6a914e40ee6182
DanielleJeanine <danielle.jeanine92@gmail.com>
2023-11-17 17:12:06 -0300

    Ajustes que deram pra fazer

--- show_do_milhao/src/pages/Ganhou/index.jsx
+++ show_do_milhao/src/pages/Ganhou/index.jsx
@@ -1,8 +1,18 @@
+import { useRef } from 'react';
 import moedas from '../../assets/imagens/moedas.gif';
-import vitoria from '../../assets/imagens/vitoria.jpeg'
+import vitoria from '../../assets/imagens/vitoria.jpeg';
+import vitoria_audio from '../../assets/sons/vitoria_audio.mp3';
 import './style.css';
 
 export default function Ganhou (){
+    const audio1 = useRef(null)
+
+    const reproduzirAudio = () => {
+        if(audio1.current){
+            audio1.current.play();
+        }
+    }
+
     return(
         <><div className='titulo'>
             <h1>PARABÉNS!!</h1>
@@ -12,7 +22,8 @@ export default function Ganhou (){
                 <img src={moedas} alt="" />
             </div>
             <div className="imagem02">
-                <img src={vitoria} alt=""/>
+                <img src={vitoria} alt="" onClick={reproduzirAudio}/>
+                <audio ref={audio1} src={vitoria_audio} type='audio/mp3'></audio>
             </div>
         </div>
             
